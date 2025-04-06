b6772fef0bd94e871c8b0cefabf94cd379990b87
Nehal Mohsen <58566870+nehalmohsen22@users.noreply.github.com>
2025-04-06 09:19:44 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import TodoInput from "./components/TodoInput"
+import TodoInput from "./components/TodoList"
+
+
+function App() {
+
+  return (
+    <main>
+      <TodoInput />
+      <TodoList />
+    </main>
+  )
+}
+
+export default App
